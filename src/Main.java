769735c5d653781826284dfdf7375ff9648565ab
SomniumDZ <mshveda28@gmail.com>
2018-11-12 20:29:16 +0200

    Controller added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Main extends Application {
     public static void main(String[] args) {
@@ -9,9 +15,9 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        BorderPane root = new BorderPane();
-        Scene scene = new Scene(root, 600, 600);
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/res/fxmls/Main.fxml"));
+        Scene scene = new Scene(root, 600, 400);
 
         primaryStage.setScene(scene);
         primaryStage.show();
