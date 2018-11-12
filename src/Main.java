3c0dc90288dbbafd501965133586b139638eec50
SomniumDZ <mshveda28@gmail.com>
2018-11-12 16:46:43 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root, 600, 600);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
