04eab7dcefa27b230c7095eb604ef2eb2e4524c8
SomniumDZ <mshveda28@gmail.com>
2018-11-15 15:44:01 +0200

    updated

--- src/controllers/events/Event.java
+++ src/controllers/events/Event.java
@@ -1,21 +1,49 @@
 package controllers.events;
 
 import controllers.nodes.Node;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 
 
 public abstract class Event<Visual extends Pane> extends Node<Visual> {
     private Event[] childEvents;
 
-    Event(double x, double y, String fxmlPath, Event[] childEventsContainer) throws IOException {
+    private FileInputStream editorFile;
+    private FXMLLoader editorLoader;
+    private BorderPane editor;
+    private FOptionalEventController controller;
+
+    Event(double x, double y, String fxmlPath, String editorPath, Event[] childEventsContainer) throws IOException {
         super(x,y,fxmlPath);
 
+        editorLoader = new FXMLLoader();
+        editorFile = new FileInputStream(editorPath);
+        editor = editorLoader.load(editorFile);
+
+        controller = editorLoader.getController();
+
         childEvents = childEventsContainer;
+
+        getVisual().setOnMouseClicked(event -> {
+            if (event.getClickCount() == 2){
+                new EventEditor(editor);
+            }
+        });
+    }
+
+    public BorderPane getEditor() {
+        return editor;
     }
 
     public Event[] getChildEvents() {
         return childEvents;
+    }
+
+    public FOptionalEventController getController() {
+        return controller;
     }
 }
--- src/controllers/events/FOptionalEvent.java
+++ src/controllers/events/FOptionalEvent.java
@@ -1,31 +1,13 @@
 package controllers.events;
 
-import javafx.fxml.FXMLLoader;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 
 public class FOptionalEvent extends Event<GridPane> {
-    private FileInputStream editorFile;
-    private FXMLLoader editorLoader;
-    private BorderPane editor;
-    private FOptionalEventController controller;
-
 
     public FOptionalEvent(double x, double y) throws IOException {
-        super(x, y, "fxmls/4OptionsEvent.fxml", new Event[4]);
-        controller = new FOptionalEventController();
+        super(x, y, "fxmls/4OptionsEvent.fxml", "fxmls/4OptionsEventEditor.fxml", new Event[4]);
 
-        editorLoader = new FXMLLoader();
-        editorFile = new FileInputStream("fxmls/4OptionsEventEditor.fxml");
-        editor = editorLoader.load(editorFile);
-
-        getVisual().setOnMouseClicked(event -> {
-            if (event.getClickCount() == 2){
-                new EventEditor(editor);
-            }
-        });
     }
 }
