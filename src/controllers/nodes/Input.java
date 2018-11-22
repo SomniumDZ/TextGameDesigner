3f030f0929af96069486d17ae37154d04aa62730
SomniumDZ <mshveda28@gmail.com>
2018-11-22 23:53:58 +0200

    Node input class added

--- src/controllers/nodes/Input.java
+++ src/controllers/nodes/Input.java
@@ -1,4 +1,22 @@
 package controllers.nodes;
 
-public class Input {
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.GridPane;
+
+import java.io.IOException;
+
+import static main.Main.ew;
+
+public class Input extends GridPane {
+    public Input() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxmls/Input.fxml"));
+        loader.setRoot(this);
+        loader.setController(this);
+        try {
+            loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+            ew.throwError("FXML read error");
+        }
+    }
 }
