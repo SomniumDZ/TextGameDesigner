047e0870fe98f2a429091250661944c7086760fd
SomniumDZ <mshveda28@gmail.com>
2019-03-14 18:26:22 +0200

    LocationContextMenu added

--- src/unnamed/Location.java
+++ src/unnamed/Location.java
@@ -1,8 +1,9 @@
 package unnamed;
 
-import controllers.LocationViewController;
+import controllers.LocationController;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -22,6 +23,8 @@ public class Location {
 
     private String UID;
 
+    private ContextMenu contextMenu;
+
     public Location(String UID, ComboBox<String> locationComboBox) {
         this.UID = UID;
         roadMap = new ArrayList<>();
@@ -32,25 +35,34 @@ public class Location {
         VBox view = new VBox();
         loader = new FXMLLoader();
         loader.setRoot(view);
+        FXMLLoader contextMenuLoader = new FXMLLoader();
+        contextMenu = new ContextMenu();
+        contextMenuLoader.setRoot(contextMenu);
         try {
             loader.load(new FileInputStream("fxmls/LocationView.fxml"));
+            contextMenuLoader.setController(loader.getController());
+            contextMenuLoader.load(new FileInputStream("fxmls/LocationContextMenu.fxml"));
         } catch (IOException e) {
             e.printStackTrace();
             ew.throwError("Location fxml loader error");
         }
-        ((LocationViewController)loader.getController()).getLocationViewLabel().setText(UID);
+        ((LocationController)loader.getController()).initialization();
+        ((LocationController)loader.getController()).getLocationViewLabel().setText(UID);
+        ((LocationController)loader.getController()).setLocation(this);
 
         Main.getMainController().getLocationsPane().getChildren().add(
                 Main.getMainController().getLocationsPane().getChildren().size()-1,
                 view
         );
+
+        canvas.setOnContextMenuRequested(event -> contextMenu.show(canvas, event.getScreenX(), event.getScreenY()));
     }
 
     public Location(){}
 
     public Location(String name, ComboBox<String> locationChoiceBox, Image image) {
         this(name, locationChoiceBox);
-        ((LocationViewController) loader.getController()).getLocationViewImage().setImage(image);
+        ((LocationController) loader.getController()).getLocationViewImage().setImage(image);
     }
 
     public Pane getCanvas() {
