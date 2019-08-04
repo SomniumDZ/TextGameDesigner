befbe4a06cb04b75662466acf43115b07e608f48
SomniumDZ <mshveda28@gmail.com>
2019-08-04 09:46:18 +0300

    sequence node was redesigned

--- src/сore/nodes/SequenceNode.java
+++ src/сore/nodes/SequenceNode.java
@@ -2,15 +2,19 @@ package сore.nodes;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import service.ModalWindow;
 
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class SequenceNode extends BorderPane {
     @FXML
     HBox header;
+    @FXML
+    Label title;
 
     public SequenceNode() {
         FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxmls/SequenceNode.fxml"));
@@ -27,9 +31,17 @@ public class SequenceNode extends BorderPane {
     }
 
     private void initDraggability() {
+        //sequence pane in scene coordinates
+        AtomicReference<Double> deltaX = new AtomicReference<>(0d);
+        AtomicReference<Double> deltaY = new AtomicReference<>(0d);
+        header.setOnMousePressed(event -> {
+            deltaX.set(getLayoutX()-event.getSceneX()+getTranslateX());
+            deltaY.set(getLayoutY()-event.getSceneY()+getTranslateY());
+            toFront();
+        });
         header.setOnMouseDragged(event -> {
-            setTranslateX(event.getSceneX());
-            setTranslateY(event.getSceneY());
+            setTranslateX(event.getSceneX()+deltaX.get());
+            setTranslateY(event.getSceneY()+deltaY.get());
         });
     }
 
